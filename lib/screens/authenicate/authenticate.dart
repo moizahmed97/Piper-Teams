0d93936418e242079cbfd6fb0f67469d94f33753
mateen ahmed <moizahmed97@gmail.com>
2020-05-27 00:02:05 +0300

    make register page the first one

--- lib/screens/authenicate/authenticate.dart
+++ lib/screens/authenicate/authenticate.dart
@@ -9,7 +9,7 @@ class Authenticate extends StatefulWidget {
 }
 
 class _AuthenticateState extends State<Authenticate> {
-  bool showSignIn = true;
+  bool showSignIn = false;
 
   void toggleView() {
     setState(() => showSignIn = !showSignIn);
