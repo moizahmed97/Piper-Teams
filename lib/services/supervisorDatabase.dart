8f8a60ed8cd6762964407ba87b6091552fb905db
mateen ahmed <moizahmed97@gmail.com>
2020-06-03 00:19:07 +0300

    change app name for android

--- lib/services/supervisorDatabase.dart
+++ lib/services/supervisorDatabase.dart
@@ -1,3 +1,10 @@
 class SupervisorDatabaseServices {
+//TODO Move stuff here and organize them
 
+// TODO Fix font size for manage tram mameber
+
+// TODO Fix font size for plan progress percent
+
+
+// TODO Fix text wrap for name of team member in user swithcuinbg page 
 }
