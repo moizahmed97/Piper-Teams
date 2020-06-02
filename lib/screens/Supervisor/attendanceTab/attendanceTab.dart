5f181742a546485aa5e96d3ad244359b6350c6d2
mateen ahmed <moizahmed97@gmail.com>
2020-06-02 13:36:22 +0300

    modify members tab page

--- lib/screens/Supervisor/attendanceTab/attendanceTab.dart
+++ lib/screens/Supervisor/attendanceTab/attendanceTab.dart
@@ -13,72 +13,52 @@ class _AttendanceTabState extends State<AttendanceTab> {
   Widget build(BuildContext context) {
     // Get the Student Document that has Name and ID in each document
     final students = Provider.of<List<SimpleTeamMemberInfo>>(context);
-     students.removeWhere(
-                (element) => element.teamMemberID == "Placeholder teamMemberID");
-    if (students.length !=0) {
+    students.removeWhere(
+        (element) => element.teamMemberID == "Placeholder teamMemberID");
+    if (students.length != 0) {
       return Container(
-      child: ListView.builder(
-          itemCount: students.length,
-          itemBuilder: (context, index) {
-            return Card(
-              elevation: 4.0,
-              color: attendanceColor(attendances[index]),
-              margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-              child: ListTile(
-                  dense: true,
-                  onLongPress: () {
-                    setState(() {
-                      attendances[index] = 0;
-                    });
-                  },
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  leading: IconButton(
-                      icon: Icon(
-                        Icons.clear,
-                        size: 35,
-                        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text("You have a ${students.length} Members in your team", style: Theme.of(context).textTheme.subtitle1,),
+            ),
+            ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: students.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 4.0,
+                    color: Colors.teal[700],
+                    margin: new EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 6.0),
+                    child: ListTile(
+                      dense: true,
+                      onLongPress: () {},
+                      contentPadding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      title: Center(
+                        child: Text(
+                          '${students[index].name}',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
                       ),
-                      onPressed: () {
-                        setState(() {
-                          attendances[index] = 2;
-                        });
-                      }),
-                  title: Center(
-                    child: Text(
-                      '${students[index].name}',
-                      style: Theme.of(context).textTheme.headline6,
                     ),
-                  ),
-                  trailing: IconButton(
-                      icon: Icon(
-                        Icons.check,
-                        size: 35,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          attendances[index] = 1;
-                        });
-                      })),
-            );
-          }),
-    );
+                  );
+                }),
+          ],
+        ),
+      );
     } else {
       return Container(
-        child: Center(child: Text("You do not have any Students yet", style: TextStyle(fontSize: 20),),),
+        child: Center(
+          child: Text(
+            "You do not have any Students yet",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
       );
     }
-  }
-
-  final List<int> attendances = <int>[1, 2, 1, 2, 0, 0, 1, 2, 0];
-
-  Color attendanceColor(int attendance) {
-    if (attendance == 1)
-      return Colors.teal;
-    else if (attendance == 2)
-      return Colors.orange[600];
-    else
-      return Colors.grey[600];
   }
 }
